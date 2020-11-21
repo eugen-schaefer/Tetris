8533138002bed4424f6018bf4ff1e8dcedea6217
Eugen Schaefer <eugen.schaefer1979@gmail.com>
2020-11-21 16:41:44 +0100

    Document Dashboard class

--- src/Dashboard.h
+++ src/Dashboard.h
@@ -9,23 +9,39 @@
 
 using TetrominoQueueType = std::deque<std::unique_ptr<TetrominoGraphic>>;
 
+/// Dashboard provides information to the player about the current game's state.
+/// It shows the queue of the upcoming shapes being drawn as the next elements
+/// after the currently active shape is locked down.
 class Dashboard : public sf::Drawable {
    public:
     Dashboard() = default;
 
-    // offset_window_top_border:  distance between the window top edge and the
-    //                            top of the dashboard
-    // available_width:           maximum width between the window left edge and
-    //                            the grid left edge
+    /// Constructs a dashboard consisting of two rectangles with equal widths.
+    /// While one rectangle contains the queue of the next elements being drawn,
+    /// the other rectangle contains the scoring information.
+    /// \param offset_window_top_border: distance between the window top edge
+    ///                                  and the top of the dashboard.
+    /// \param available_width: maximum width between the window left edge and
+    ///                         the grid left edge
+    /// \param max_available_height: Maximum available space that can be used by
+    /// the dashboard
+    /// \param font: Font for the characters
     Dashboard(float offset_window_top_border, float available_width,
               float max_available_height, sf::Font& font);
 
+    /// Inserts new tossed shapes to the bottom of the dashboard queue while
+    /// shifting the existing ones to the top.
+    /// \param shape: shape being inserted.
     void InsertNextTetromino(TetrominoType shape);
 
+    /// Increases the curent score-counter by score
     void AddToScore(int score);
 
+    /// Increases the counter of cleared lines by nr_cleared_lines
     void AddToClearedLines(int nr_cleared_lines);
 
+    /// Resets the dashboard (clear all numbers, empty the queue) for a new
+    /// game.
     void Reset();
 
    private:
